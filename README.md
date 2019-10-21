8d8b57ad6d147ca7ef98c1a2ec4a17f66a2a195b
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-10-21 12:29:02 -0400

    new template

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# template_choclat
+# template_chocolat
+
 New design theme and based on chocolat theme.
+If you have included new modules, you must update in consequence your files and css 
+You must just copy the files that you want change inside the template chocolat directory.
+
+
+This module contains
+
+- The language files in English and French
+- The css file in French and English
+- The module
+  
+Via the installation system administration ClicShopping
+
+Technical Prerequisites: None
+
+License : GPL 2 - MIT
+
+Modules:
+
+- Compatibility: >= version 3.0
+- Multi languages
+
+Recommendation and documentation specific use :
+The module create a new directory inside the template directory
+All the new modules inserted inside the Default template will work with chocolat template.
+If you must make an update, please, copy the files inside the chocolat template.
+
+
+Implementation:
+
+- Copy the All Diretories inside Shop expcept ModuleInfosJson
+- Copy the template_chocolat.json into ClicShopping/Work/Cache/Github (manual installation)
+- In your administration : Design / Configuration / Configuration : Change the website theme
+
+
+
+ All informations about the ClicShopping
+ 
+ Download ClicShopping : https://github.com/ClicShopping/ClicShopping_V3/archive/master.zip
+
+ Community : https://www.clicshopping.org
+
+ Software : https://github.com/ClicShopping
+
+ Official add on : https://github.com/ClicShoppingOfficialModulesV3
+
+ Community add on : https://github.com/ClicShoppingV3Community
+
+ trademark License info : https://www.clicshopping.org/forum/trademark/ 
+ 
+
+![image](https://github.com/ClicShoppingV3Community/template_chocolat/blob/master/ModuleInfosJson/image.png)
---
+++ sources/template/Chocolat/footer.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ *
+ *  @copyright 2008 - https://www.clicshopping.org
+ *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
+ *  @Licence GPL 2 & MIT
+ *  @licence MIT - Portion of osCommerce 2.4
+ *  @Info : https://www.clicshopping.org/forum/trademark/
+ *
+ */
+   use ClicShopping\OM\CLICSHOPPING;
+   use ClicShopping\OM\Registry;
+
+   $CLICSHOPPING_Hooks = Registry::get('Hooks');
+   $CLICSHOPPING_Template = Registry::get('Template');
+?>
+      </div><!-- end bodyContent -->
+
+<?php
+  if ($CLICSHOPPING_Template->hasBlocks('boxes_column_left')) {
+?>
+      <div id="columnLeft" class="col-sm-6 col-lg-<?php echo $CLICSHOPPING_Template->getGridColumnWidth(); ?> order-xs-2 order-lg-1">
+        <?php echo $CLICSHOPPING_Template->getBlocks('boxes_column_left'); ?>
+      </div>
+<?php
+  }
+
+  if ($CLICSHOPPING_Template->hasBlocks('boxes_column_right')) {
+?>
+      <div id="columnRight" class="col-sm-6 col-lg-<?php echo $CLICSHOPPING_Template->getGridColumnWidth(); ?> order-xs-3 order-lg-3">
+        <?php echo $CLICSHOPPING_Template->getBlocks('boxes_column_right'); ?>
+      </div>
+<?php
+  }
+?>
+      </div><!-- end flex -->
+<?php //important before block ?>
+      <footer class="page-footer" id="footer">
+        <div class="hr footerHr"></div>
+        <div class="footer"><?php echo $CLICSHOPPING_Template->getBlocks('modules_footer'); ?></div>
+        <div class="footerSuffix"><?php echo $CLICSHOPPING_Template->getBlocks('modules_footer_suffix'); ?></div>
+      </footer>
+    </div> <!-- BodyWrapper -->
+  </div> <!-- container //-->
+<?php
+    echo $CLICSHOPPING_Template->getBlocks('footer_scripts');
+
+    $source_folder = CLICSHOPPING::getConfig('dir_root', 'Shop') . 'includes/Module/Hooks/Shop/Footer/';
+
+    if (is_dir($source_folder)) {
+      $files_get_output = $CLICSHOPPING_Template->getSpecificFiles($source_folder, 'FooterOutput*');
+      $files_get_call = $CLICSHOPPING_Template->getSpecificFiles($source_folder, 'FooterCall*');
+
+      foreach ($files_get_output as $value) {
+        if (!empty($value['name'])) {
+          echo $CLICSHOPPING_Hooks->output('Footer', $value['name'], null, 'display');
+        }
+      }
+
+      foreach ($files_get_call as $value) {
+        if (!empty($value['name'])) {
+          echo $CLICSHOPPING_Hooks->call('Footer', $value['name']);
+        }
+      }
+    }
+?>
+  </body>
+</html>
